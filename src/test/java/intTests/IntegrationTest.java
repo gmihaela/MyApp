9e1a33b14c6616eb3f51b52c7f4ea3a42e581499
Dumitru Neacsu <Dumitru.Neacsu@endava.com>
2018-11-21 14:10:31 +0200

    repo

--- src/test/java/intTests/IntegrationTest.java
+++ src/test/java/intTests/IntegrationTest.java
@@ -14,12 +14,12 @@ public class IntegrationTest {
 
     public void TestTitle1() {
 
-        File src = new File("C:/Users/dneacsu/Desktop/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+        File src = new File("/usr/local/bin/phantomjs");
         System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
         WebDriver driver = new PhantomJSDriver();
-        driver.get("http://192.168.39.31:8080/");
+        driver.get("http://192.168.39.31:8989/AMGrads");
         String title1 = driver.getTitle();
-        Assert.assertTrue(title1.contains("Jenkins"));
+        Assert.assertTrue(title1.contains("MyApp"));
 
 
     }
